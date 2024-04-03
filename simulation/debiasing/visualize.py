e88dfb67a3605dff170819676e56d5ec453347d1
Anastasios N. Angelopoulos <angelopoulos@berkeley.edu>
2024-04-02 21:52:42 -0700

    [raw iterates]

--- simulation/debiasing/visualize.py
+++ simulation/debiasing/visualize.py
@@ -24,7 +24,7 @@ def __(np, os, pd):
     df = pd.concat(dfs, ignore_index=False)
     df = df.reset_index(names="time")
     df['parameters'] = [r'$\eta=$' + str(row['lr']) + r', $\kappa=$' + str(row['viscosity']) for _, row in df.iterrows()]
-    df['norm_avg_grad'] = df.average_gradient.apply(np.linalg.norm)
+    df['norm_avg_grad'] = df.average_gradient.apply(np.linalg.norm, ord=np.inf)
     df['theta_1'] = df.theta.apply(lambda th : th[0])
     df['theta_2'] = df.theta.apply(lambda th : th[1])
     df['y_1'] = df.y.apply(lambda y : y[0])
@@ -81,10 +81,9 @@ def __(T, df, np, plt, sns):
                 alpha=1
             )
             B_theta = np.nan_to_num(_to_plot.oracle_norm_thetaT_theta0,100).max()
-            print(B_theta)
             # Also plot the theoretical bound
             bound= B_theta/(min_eta * _t)
-            _lp.plot(_t[100:], bound[100:], 'k--', linewidth=3, label=r'Bound')
+            _lp.plot(_t[100:], bound[100:], 'k--', linewidth=3, label=r'bound')
             _axs[_i,_j].set_ylabel("Norm avg grad (drift=" + str(shifts_to_plot[_i]) + ')')
             _axs[_i,_j].set_xlabel("Time (T)")
             _axs[_i,_j].legend(loc='upper right')
@@ -116,50 +115,6 @@ def __(
     shifts_to_plot,
     sns,
 ):
-    # Make plot of the signed average gradient
-    _fig, _axs = plt.subplots(nrows=len(shifts_to_plot), ncols=len(kappas_to_plot), figsize=(len(kappas_to_plot)*10,5*len(shifts_to_plot)), sharex="col", sharey="row")
-    _t = np.arange(T)+1
-
-    for _i in range(len(shifts_to_plot)):
-        for _j in range(len(kappas_to_plot)):
-            _to_plot = df[
-                df.lr.isin(etas_to_plot) &
-                (df.viscosity==kappas_to_plot[_j]) & 
-                (df.distribution_shift_speed==shifts_to_plot[_i]) & 
-                (df.time % plot_every == 0)
-            ].sort_values(["lr", "viscosity"], ascending=False)
-            _lp = sns.lineplot(
-                ax=_axs[_i,_j], 
-                data=_to_plot,
-                x="time", 
-                y="g_1", 
-                hue="parameters", 
-                linewidth=1, 
-                alpha=0.5
-            )
-
-            # Also plot the theoretical bound
-            #_lp.plot(_t, 1/np.sqrt(_t), 'k--', linewidth=3, label=r'$\frac{1}{\sqrt{T}}$')
-            _axs[_i,_j].set_ylabel("Avg grad (drift=" + str(shifts_to_plot[_i]) + ')')
-            _axs[_i,_j].set_xlabel("Time (T)")
-
-    plt.tight_layout()
-    plt.gca()
-    return
-
-
-@app.cell
-def __(
-    T,
-    df,
-    etas_to_plot,
-    kappas_to_plot,
-    np,
-    plot_every,
-    plt,
-    shifts_to_plot,
-    sns,
-):
     # Now plot the iterates and y_t values for the first d=2 dimensions
     _fig, _axs = plt.subplots(nrows=len(shifts_to_plot), ncols=len(kappas_to_plot), figsize=(len(kappas_to_plot)*10,5*len(shifts_to_plot)), sharex="col", sharey="row")
     for _i in range(len(shifts_to_plot)):
@@ -167,7 +122,7 @@ def __(
             _to_plot = df[
                 df.lr.isin(etas_to_plot) & 
                 (df.viscosity==kappas_to_plot[_j]) & 
-                (df.distribution_shift_speed==shifts_to_plot[_i]) &
+                (df.trend_period==shifts_to_plot[_i]) &
                 (df.time % plot_every == 0)
             ].sort_values("lr", ascending=False)
             normalized_time = np.arange(T//plot_every)/(T//plot_every)
@@ -179,16 +134,6 @@ def __(
                 hue="parameters", 
                 #c=normalized_time,
                 alpha=0.5
-            )
-            sns.scatterplot(
-                ax=_axs[_i,_j], 
-                data=_to_plot[_to_plot.lr == etas_to_plot[0]], 
-                x="y_1", 
-                y="y_2", 
-                c=normalized_time, 
-                cmap="Grays", 
-                alpha=0.1, 
-                zorder=-1
             )
             if _j == 0:
                 _axs[_i,_j].set_ylabel(r'$\theta_2$ (drift$=$' + str(shifts_to_plot[_i]) + ')')
