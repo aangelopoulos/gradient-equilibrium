e1a34f09922e99a48863befe666c4218207b4a1e
Anastasios N. Angelopoulos <angelopoulos@berkeley.edu>
2024-11-26 21:32:00 +0000

    [add ExpGD]

--- core/algorithms.py
+++ core/algorithms.py
@@ -56,6 +56,57 @@ class GD(torch.optim.Optimizer):
                 p.data = p.data - group['lr'] * d_p
 
         return loss
+    
+# Define the gradient descent optimizer
+class ExpGD(torch.optim.Optimizer):
+    def __init__(self, params, lr=1e-3, penalty_type=None, lambda_=0.0, alpha=0):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if penalty_type not in (None, 'L1', 'L2'):
+            raise ValueError("Invalid penalty type: {}".format(penalty_type))
+        if lambda_ < 0.0:
+            raise ValueError("Invalid penalty level (lambda): {}".format(lambda_))
+        if not (0 <= alpha <= 1.0):
+            raise ValueError("Alpha must be in the range [0, 1.0].")
+
+        # Store initial learning rate separately to apply decay formula
+        self.global_step = 0
+        defaults = dict(lr=lr, initial_lr=lr, penalty_type=penalty_type, lambda_=lambda_, alpha=alpha)
+        super(GD, self).__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step."""
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            lr = group['initial_lr']
+            alpha = group['alpha']
+
+            # Decay the learning rate proportional to 1 / t^alpha
+            self.global_step += 1
+            decayed_lr = lr / (self.global_step ** alpha)
+            group['lr'] = decayed_lr
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                d_p = p.grad
+
+                # L1 or L2 regularization
+                if group['penalty_type'] == 'L2':
+                    d_p = d_p + group['lambda_'] * p.data
+                elif group['penalty_type'] == 'L1':
+                    d_p = d_p + group['lambda_'] * p.data.sign()
+
+                p.data *= torch.exp(-group['lr'] * d_p)
+                p.data /= p.data.sum()
+
+        return loss
 
 # Simple debiasing model
 class DebiasingModel(nn.Module):
@@ -86,3 +137,11 @@ class LogisticModel(nn.Module):
 
     def forward(self, x_t):
         return torch.sigmoid(x_t @ self.theta)
+
+# Ensembling quantile model
+class EnsemblingModel(nn.Module):
+    def __init__(self, init_weights):
+        self.weights = nn.Parameter(init_weights)
+
+    def forward(self, predictors):
+        return self.weights @ predictors
