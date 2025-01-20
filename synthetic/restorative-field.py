bb1a4c3c2546718148047f012f5d59c04daca7db
Ryan Tibshirani <ryantibs@gmail.com>
2025-01-19 21:49:24 -0800

    Improve restorative fig

--- synthetic/restorative-field.py
+++ synthetic/restorative-field.py
@@ -9,21 +9,34 @@ sns.set_style("white")
 plt.figure(figsize=(10, 10))
 
 # Create grid points
-x = np.linspace(-4, 4, 20)
-y = np.linspace(-4, 4, 20)
+B = 4
+x = np.linspace(-B, B, 20)
+y = np.linspace(-B, B, 20)
 X, Y = np.meshgrid(x, y)
 
 # Calculate radius at each point
 R = np.sqrt(X**2 + Y**2)
 
 # Define horizon
-h = 2
+h = 2.15
 
 # Calculate field components
 # Inside horizon: zero field
 # Outside horizon: point inward with magnitude 1/r
-U = np.where(R > h+0.3, -X/R, 0)
-V = np.where(R > h+0.3, -Y/R, 0)
+U = np.where(R > h, -X/R, 0)
+V = np.where(R > h, -Y/R, 0)
+
+scale = 20
+Rho = np.sqrt(U**2 + V**2)
+Rho = np.where(Rho > 0, Rho, 1)
+XplusU = X + U / Rho * 2*B/scale
+YplusV = Y + V / Rho * 2*B/scale
+T = np.sqrt(XplusU**2 + YplusV**2) # Radius of arrowhead
+Numer = np.where((R > h) & (T < h), R - h, 1)
+Denom = np.where((R > h) & (T < h), R - T, 1)
+Factor = Numer / Denom
+U = U / Rho * 2*B/scale * Factor
+V = V / Rho * 2*B/scale * Factor
 
 # Plot the field lines
 idx_nonzero = (np.abs(U) > 0) & (np.abs(V) > 0)
@@ -36,13 +49,12 @@ V = V[idx_nonzero]
 plt.axhline(y=0, color='gray', linestyle='-', alpha=0.3)
 plt.axvline(x=0, color='gray', linestyle='-', alpha=0.3)
 
-# Plot horizon circle and quivers
+# Plot quivers
+plt.quiver(X, Y, U, V, scale=1, scale_units='xy', angles='xy', width=0.0027, color='#EBB901', headaxislength=5, headwidth=4, pivot='tail')
+
+# Plot horizon circle
+h = 2
 theta = np.linspace(0, h*np.pi, 97)
-theta_arrows = theta[::3]
-theta2_arrows = theta[::2]
-# plt.quiver(h*np.cos(theta_arrows), h*np.sin(theta_arrows), -np.cos(theta_arrows), -np.sin(theta_arrows), scale=20, width=0.0027, color='#EBB901', headaxislength=5, headwidth=4, pivot='tip')
-# plt.quiver(1.3*h*np.cos(theta2_arrows), 1.3*h*np.sin(theta2_arrows), -np.cos(theta2_arrows), -np.sin(theta2_arrows), scale=20, width=0.0027, color='#EBB901', headaxislength=5, headwidth=4, pivot='tip')
-plt.quiver(X, Y, U, V, scale=20, width=0.0027, color='#EBB901', headaxislength=5, headwidth=4, pivot='tail')
 plt.plot(h*np.cos(theta), h*np.sin(theta), 'k', linewidth=1.5, linestyle='dotted')
 
 # Set limits and aspect ratio
